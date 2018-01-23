a83bb2c89daf2cb85dc7f699de07674ef9608420
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-23 01:53:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function viewCart() {
    for(var x = 0; x < cart.length; x++){
      for (var items in cart[x]){
        
-       cartList += 
+       cartList += `${items} at $${}`; 
      }
    }
  }
