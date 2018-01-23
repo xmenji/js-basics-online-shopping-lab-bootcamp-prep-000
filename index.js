9884fdf2779c47cf8d9ce7d1b0d2e93d3713e2bf
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-23 02:02:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,18 +20,27 @@ function addToCart(item) {
 }
 
 function viewCart() {
-   if (cart.length === 0) {
-     console.log("Your shopping cart is empty.")
-   } else {
-     var items = [];
-     for (var i = 0; i < cart.length; i++) {
-       for (var item in cart[i]) {
-         items.push(item + " at $" + cart[i][item])
-       }
-     }
-     console.log("In your cart, you have " + items.join(", ") + ".");
-   }
- }
+  var cartItems = [];
+  if (cart.length === 0) {
+    console.log('Your shopping cart is empty.')
+
+  } else if (cart.length === 1) {
+    var printOneItem = `In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`
+    console.log(printOneItem)
+
+  } else if (cart.length === 2) {
+    var printTwoItems = `In your cart, you have ${Object.keys(cart[cart.length-2])} at $${cart[cart.length-2][Object.keys(cart[cart.length-2])]} and ${Object.keys(cart[cart.length-1])} at $${cart[cart.length-1][Object.keys(cart[cart.length-1])]}.`
+    console.log(printTwoItems)
+  
+    
+  } else {
+      for (var i = 0; i < cart.length-1; i++) {
+      cartItems.push(` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`)
+          }
+          var printThreeOrMoreItems = `In your cart, you have${cartItems}, and ${Object.keys(cart[cart.length-1])} at $${cart[cart.length-1][Object.keys(cart[cart.length-1])]}.`
+          console.log(printThreeOrMoreItems)
+  }
+}
 
 function total() {
   // write your code here
