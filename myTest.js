5b6b22b6062cbe287655df57ee49a41f55d870c0
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-23 01:51:06 +0000

    automatically backed up by learn

--- myTest.js
+++ myTest.js
@@ -34,3 +34,25 @@ function viewCart() {
 
 
 viewCart();
+
+
+
+/*
+ if(cart.length !== 0){
+    
+    for(var x = 0; x < cart.length; x++){
+      obj = cart[x];
+    
+      if(cart.length > 1 && x === cart.length - 1){
+        itemsInCart += ' and';
+      }
+      
+      itemsInCart += ` ${Object.keys(obj)} at $${obj[Object.keys(obj)]}`;
+      
+      if(cart.length > 2 && x !== cart.length - 1){
+        itemsInCart += ',';
+      }
+      
+    }
+    console.log(`${itemsInCart}.`);
+*/
