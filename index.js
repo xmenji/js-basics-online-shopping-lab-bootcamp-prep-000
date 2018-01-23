f734e8053c0bcec3b1bc9736052e50a54001ab90
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-23 02:41:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,4 +71,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(cardNumber !== ''){
+    console.log(`Your total cost is ${total()}, which will be charged to the cart ${cardNumber}.`);
+  }
 }
