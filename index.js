5f6b3b7f5755f01a560e1b6e5b33b371d6bfb8c7
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-23 02:43:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,5 +73,9 @@ function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber !== ''){
     console.log(`Your total cost is ${total()}, which will be charged to the cart ${cardNumber}.`);
+    cart.slice(cart.length);
+  }
+  else{
+    console.log("Sorry, we don't have a credit card on file for you.");
   }
 }
